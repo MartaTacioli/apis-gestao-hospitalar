7c2dd1d7b3e10d5c8a0444d96b7ba21a5e1b7c6f
Martacioli <martatacioli1@gmail.com>
2021-05-02 20:26:50 -0300

    Listar Centro de Custo

--- src/main/java/com/gestaoclinica/apis/entities/CentroDeCusto.java
+++ src/main/java/com/gestaoclinica/apis/entities/CentroDeCusto.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -22,6 +23,7 @@ public class CentroDeCusto implements Serializable{
 	private String bloco;
 	private String detalhes;
 	@NotNull
+	@OneToOne
 	private Sala sala;
 	@NotNull
 	private String cnpj;
